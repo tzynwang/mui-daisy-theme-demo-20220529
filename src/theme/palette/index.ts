a5012d882d2c66d582c8cbbe82fc3d3e23b81785
Tzu Yin <tzyn.wang@gmail.com>
2022-05-25 17:28:48 +0800

    [feat] Update theme palette

---
+++ src/theme/palette/index.ts
@@ -0,0 +1,23 @@
+export default {
+  palette: {
+    primary: {
+      main: '#095CA8'
+    },
+    error: {
+      main: '#B20000'
+    },
+    success: {
+      main: '#4DA453'
+    },
+    warning: {
+      main: '#F06F34'
+    },
+    disabled: {
+      1: '#EAEAEB',
+      2: '#CBCCCD',
+      3: '#BDBDBD',
+      4: '#96989C'
+    },
+
+  }
+};
