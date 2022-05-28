187e3e6ba44fd2561ce6ef4e3a28a61e365bee83
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 19:47:37 +0800

    [feat] Implement @Models/Mode

---
+++ src/models/Mode/types.d.ts
@@ -0,0 +1,6 @@
+export type { ThemeOptions as MuiThemeOptions } from '@mui/system';
+export type { Theme as MuiTheme } from '@mui/material/styles';
+
+export type ThemeKeys = 'cupcake' | 'coffee';
+
+export type ThemePool = Array<{ key: ThemeKeys; theme: Theme }>;
