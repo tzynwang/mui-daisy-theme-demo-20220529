33c1aa65cb8657ca4d543292ba6475d714e0a744
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 21:40:11 +0800

    [perf] Update @Models/Mode/types

--- src/models/Mode/types.d.ts
+++ src/models/Mode/types.d.ts
@@ -1,11 +1,13 @@
-import { THEME_KEYS } from './index';
-export type { ThemeOptions as MuiThemeOptions } from '@mui/system';
-export type { Theme as MuiTheme } from '@mui/material/styles';
+import { THEME_KEYS } from '@Models/Mode';
+import type { ThemeOptions } from '@mui/system';
+import type { Theme as MuiTheme } from '@mui/material/styles';
 
 export type ThemeKeys = typeof THEME_KEYS[number];
 
 export type ThemePool = Array<{ key: ThemeKeys; theme: MuiTheme }>;
 
 export interface ThemeModule {
-  default: MuiThemeOptions;
+  default: ThemeOptions;
 }
+
+export { MuiTheme };
