f9fac451774fd8ed52b157c2e5b06c043a1faf91
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 12:57:04 +0800

    [perf] Move ThemeToggle into @Components/Common

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+
 import { ThemeProvider, createTheme, Theme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
-import theme from '@Theme/index';
-import ThemeToggle from '@Components/ThemeToggle';
+
 import ModeContext from '@Components/Base/ModeContext';
+import ThemeToggle from '@Components/Common/ThemeToggle';
+import theme from '@Theme/index';
 
 function App(): React.ReactElement {
   const [mode, setMode] = useState<'light' | 'dark'>('light');
--- src/components/Common/ThemeToggle/index.tsx
+++ src/components/Common/ThemeToggle/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useContext } from 'react';
-import { ModeContext } from '@Components/App';
+import ModeContext from '@Components/Base/ModeContext';
 
 function ThemeToggle(): React.ReactElement {
   const context = useContext(ModeContext);
