3a0c401235e1d0b23dd2998cdb60640e252eb447
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 21:21:50 +0800

    [feat] Update ThemeToggle to select menu

--- src/components/Common/ThemeToggle/index.tsx
+++ src/components/Common/ThemeToggle/index.tsx
@@ -1,9 +1,11 @@
 import React, { memo, useContext } from 'react';
 
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import NativeSelect from '@mui/material/NativeSelect';
 
-import { ModeContext } from '@Models/Mode';
+import { ModeContext, THEME_KEYS } from '@Models/Mode';
 import type { ThemeKeys } from './types';
 
 function ThemeToggle(): React.ReactElement {
@@ -11,18 +13,40 @@ function ThemeToggle(): React.ReactElement {
   const context = useContext(ModeContext);
 
   /* Functions */
-  const handleThemeToggle = (): void => {
-    const currentMode = context?.mode as ThemeKeys;
-    context?.setMode(currentMode === 'cupcake' ? 'coffee' : 'cupcake');
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const key = e.target.value as ThemeKeys;
+    context?.setMode(key);
   };
 
   /* Main */
   return (
-    <Grid container alignItems="center" spacing={2}>
-      <Grid item>
-        <Button onClick={handleThemeToggle}>change theme</Button>
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      p={2}
+    >
+      <Grid item xs={6} sm={3}>
+        <InputLabel htmlFor="selectThemeKey">select a theme</InputLabel>
+        <Box>
+          <NativeSelect
+            id="selectThemeKey"
+            onChange={handleThemeChange}
+            fullWidth
+          >
+            {THEME_KEYS.map((key, index) => (
+              <option key={index} value={key}>
+                {key}
+              </option>
+            ))}
+          </NativeSelect>
+        </Box>
       </Grid>
-      <Grid item>Current theme: {context ? context.mode : null}</Grid>
+      <Grid item xs={6} sm={9}>
+        Current theme: {context ? context.mode : null}
+      </Grid>
     </Grid>
   );
 }
