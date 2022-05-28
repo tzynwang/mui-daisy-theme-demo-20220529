3e229394cfee61ebee87afdd2756c2d3d346d971
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 14:30:21 +0800

    [feat] Add theme/Coffee

---
+++ src/theme/Coffee/palette/index.ts
@@ -0,0 +1,32 @@
+const palette = {
+  palette: {
+    primary: {
+      main: '#DC944C'
+    },
+    secondary: {
+      main: '#273E40'
+    },
+    error: {
+      main: '#FC9783'
+    },
+    info: {
+      main: '#8BC9C1'
+    },
+    success: {
+      main: '#9CB686'
+    },
+    warning: {
+      main: '#FFD260'
+    },
+    text: {
+      primary: '#746D63CC',
+      secondary: '#746D63CC'
+    },
+    background: {
+      paper: '#211720',
+      default: '#211720'
+    }
+  }
+};
+
+export default palette;
