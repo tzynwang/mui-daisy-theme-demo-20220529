c03998ce0ce148c3b6ae9ca15f4c8a068d9c38a3
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 13:14:38 +0800

    [feat] ThemeToggle update layout

--- src/components/Common/ThemeToggle/index.tsx
+++ src/components/Common/ThemeToggle/index.tsx
@@ -1,22 +1,27 @@
 import React, { memo, useContext } from 'react';
+
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
 import ModeContext from '@Components/Base/ModeContext';
 
 function ThemeToggle(): React.ReactElement {
   /* States */
   const context = useContext(ModeContext);
 
+  /* Functions */
+  const handleThemeToggle = (): void => {
+    context?.setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
   /* Main */
   return (
-    <div>
-      Current theme: {context ? context.mode : null}
-      <button
-        onClick={() =>
-          context?.setMode((prev) => (prev === 'light' ? 'dark' : 'light'))
-        }
-      >
-        change theme
-      </button>
-    </div>
+    <Grid container alignItems="center" spacing={2}>
+      <Grid item>Current theme: {context ? context.mode : null}</Grid>
+      <Grid item>
+        <Button onClick={handleThemeToggle}>change theme</Button>
+      </Grid>
+    </Grid>
   );
 }
 
