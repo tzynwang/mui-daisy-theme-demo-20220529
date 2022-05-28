1770422244007cae5172aa1f59424766a6de53a6
Tzu Yin <tzyn.wang@gmail.com>
2022-05-28 13:18:32 +0800

    [perf] Componentize TypographyDemo

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 import { ThemeProvider, createTheme, Theme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
 
 import ModeContext from '@Components/Base/ModeContext';
 import ThemeToggle from '@Components/Common/ThemeToggle';
+import TypographyDemo from '@Components/Common/TypographyDemo';
 import theme from '@Theme/index';
 
 function App(): React.ReactElement {
@@ -23,16 +23,7 @@ function App(): React.ReactElement {
     <ModeContext.Provider value={{ mode, setMode }}>
       <ThemeProvider theme={dynamicTheme}>
         <CssBaseline />
-        <Typography variant="h1">h1</Typography>
-        <Typography variant="h2">h2</Typography>
-        <Typography variant="h3">h3</Typography>
-        <Typography variant="h4">h4</Typography>
-        <Typography variant="Read">
-          Typography variant="Read": Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Architecto nulla porro ea reprehenderit accusamus
-          voluptatum tenetur aspernatur sit praesentium quasi placeat aliquid
-          minus odio sequi est, non nisi. Consectetur, dolores?
-        </Typography>
+        <TypographyDemo />
         <ThemeToggle />
       </ThemeProvider>
     </ModeContext.Provider>
