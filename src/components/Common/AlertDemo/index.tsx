655f33ace6cb442be7b7c0ee470f4eca71550d0f
Tzu Yin <tzyn.wang@gmail.com>
2022-05-29 11:33:47 +0800

    [feat] Add AlertDemo

---
+++ src/components/Common/AlertDemo/index.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+function AlertDemo(): React.ReactElement {
+  return (
+    <Stack sx={{ width: '100%' }} spacing={2} p={2}>
+      <Alert severity="error">This is an error alert — check it out!</Alert>
+      <Alert severity="warning">This is a warning alert — check it out!</Alert>
+      <Alert severity="info">This is an info alert — check it out!</Alert>
+      <Alert severity="success">This is a success alert — check it out!</Alert>
+    </Stack>
+  );
+}
+
+export default memo(AlertDemo);
